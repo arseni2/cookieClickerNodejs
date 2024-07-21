fdf3c3ab59124b421d557d16b5d31ae4ad14ce74
arseni2 <arc-37@mail.ru>
2024-07-21 22:18:13 +0300

    1111

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,9 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
     @ApiProperty()
-    tgId: string;
-
-    @ApiProperty()
     username: string;
 
     @ApiProperty({nullable: true})
